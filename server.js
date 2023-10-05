3e82f263c987a0fc9b029a0f542f55e9993cbc2a
isaac k. kimura <isaackimura74@gmail.com>
2023-10-06 00:34:59 +0300

    Cors

--- server.js
+++ server.js
@@ -43,19 +43,26 @@ server.post('/login', (req, res) => {
     // Invalid credentials
     res.status(401).json({ message: 'Invalid email or password' });
   } else {
-    // Valid credentials, generate a JWT token
-    const token = jwt.sign({ userId: user.id }, jwtSecret, {
-      expiresIn: '1h', // Token expiration time (adjust as needed)
-    });
+    try {
+      // Valid credentials, generate a JWT token
+      const token = jwt.sign({ userId: user.id }, jwtSecret, {
+        expiresIn: '1h', // Token expiration time (adjust as needed)
+      });
 
-    // Respond with the generated token
-    res.status(200).json({ token });
+      console.log('Generated Token:', token);
+
+      // Respond with the generated token
+      res.status(200).json({ token });
+    } catch (error) {
+      console.error('JWT Error:', error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
   }
 });
 
 server.use(router);
 
-const port = process.env.PORT || 3500;
+const port = process.env.PORT || 8000;
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
