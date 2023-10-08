1635bc48f19d84c421847db6feb1c23a2fd18006
isaac k. kimura <isaackimura74@gmail.com>
2023-10-08 12:46:41 +0300

    Added a new admin

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const cors = require('cors');
-const jwt = require('jsonwebtoken'); // Import JWT library
+const jwt = require('jsonwebtoken');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
@@ -10,8 +10,6 @@ server.use(jsonServer.bodyParser);
 const customCorsMiddleware = require('./middlewares');
 
 server.use(cors({ origin: true, methods: 'GET,HEAD,PUT,PATCH,POST,DELETE' }));
-
-// Enable CORS for all routes
 server.use((req, res, next) => {
   console.log(`Received ${req.method} request for ${req.path}`);
   next();
@@ -20,38 +18,30 @@ server.use((req, res, next) => {
 server.use(customCorsMiddleware);
 server.use(middlewares);
 
-// Secret key for JWT token (you should use a strong, unique secret key in production)
 const jwtSecret = 'your-secret-key';
 
-// Example user data (you can load user data from a database)
-const users = [
-  {
-    id: 1,
-    email: 'admin@example.com',
-    password: 'adminpassword',
-  },
-];
+// Instead of a hardcoded user array, read users from db.json
+const db = require('./db.json');
+const users = db.admin;
 
-// Custom login route
 server.post('/login', (req, res) => {
   const { email, password } = req.body;
 
-  // Find the user by email (you would typically query a database here)
+  // Find the user by email in the 'users' array
   const user = users.find((u) => u.email === email);
 
+  console.log(user);
+
   if (!user || user.password !== password) {
-    // Invalid credentials
     res.status(401).json({ message: 'Invalid email or password' });
   } else {
     try {
-      // Valid credentials, generate a JWT token
       const token = jwt.sign({ userId: user.id }, jwtSecret, {
-        expiresIn: '1h', // Token expiration time (adjust as needed)
+        expiresIn: '1h',
       });
 
       console.log('Generated Token:', token);
 
-      // Respond with the generated token
       res.status(200).json({ token });
     } catch (error) {
       console.error('JWT Error:', error);
@@ -62,7 +52,7 @@ server.post('/login', (req, res) => {
 
 server.use(router);
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
